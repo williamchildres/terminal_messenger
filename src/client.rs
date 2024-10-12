41e2f0f1b14be41a3d5c88b56eb06a3b2528e15a
William Childres <williamrchildres@gmail.com>
2024-10-12 17:40:58 -0500

    adding tui basics, activated with /tui launch param

--- src/client.rs
+++ src/client.rs
@@ -5,9 +5,28 @@ use tokio_tungstenite::connect_async;
 use tungstenite::protocol::Message;
 use url::Url;
 
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io as std_io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
+
 // Define the asyncchronous main function
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 && args[1] == "/tui" {
+        launch_tui().await;
+        return;
+    }
     // Specify the server URL to connect to
     let server_url = Url::parse("ws://127.0.0.1:8080").unwrap();
 
@@ -48,4 +67,32 @@ async fn main() {
         _ = read_ws => (),
         _ = write_ws => (),
     }
+}
+
+async fn launch_tui() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = std_io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
