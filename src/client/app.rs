901dc5633db6b3f4244210617b5c1f048c05d607
William Childres <williamrchildres@gmail.com>
2024-10-13 02:27:24 -0500

    added message list offset so messages scroll in main display

--- src/client/app.rs
+++ src/client/app.rs
@@ -10,20 +10,20 @@ pub struct App {
     pub message_input: String, // the currently being edited message value.
     pub current_screen: CurrentScreen, // the current screen the user is looking at, and will later determine what is rendered.
     pub messages: Vec<String>,
+    pub messages_offset: usize,
 }
 
 impl App {
     pub fn new() -> App {
         App {
             message_input: String::new(),
-
             current_screen: CurrentScreen::Main,
-
             messages: Vec::<String>::new(),
+            messages_offset: 0,
         }
     }
-
     pub fn handle_websocket_message(&mut self, message: String) {
         self.messages.push(message);
+        self.messages_offset = self.messages.len().saturating_sub(10); // change 10 to whatever fits your screen height
     }
 }
