5ca58cd2d7b85a7460e324a33b227ea7fddfdb6a
William Childres <williamrchildres&gmail.com>
2024-10-13 19:25:30 -0500

    organizing server files and code

--- src/server/app.rs
+++ src/server/app.rs
@@ -1,14 +1,25 @@
+use crate::HashMap;
 pub struct App {
-    // Define your server state fields here...
+    connected_users: HashMap<String, String>,
 }
 
 impl App {
     pub fn new() -> App {
         // Initialize and return a new instance of `App`
         App {
-            // Initialize your fields here...
+            connected_users: HashMap::new(),
         }
     }
 
-    // Define other methods or functions that operate on `App` here...
+    pub async fn add_connected_user(&mut self, user_id: String, username: String) {
+        self.connected_users.insert(user_id, username);
+    }
+
+    pub async fn get_connected_user(&self, user_id: &str) -> Option<&String> {
+        self.connected_users.get(user_id)
+    }
+
+    pub async fn remove_connected_user(&mut self, user_id: &str) -> Option<String> {
+        self.connected_users.remove(user_id)
+    }
 }
