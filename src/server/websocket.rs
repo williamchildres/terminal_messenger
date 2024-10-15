4e99ea088f1624e6bfa6e8dc22385b49b611db65
William Childres <williamrchildres&gmail.com>
2024-10-14 19:31:18 -0500

    ping/pong server to client to keep connections alive

--- src/server/websocket.rs
+++ src/server/websocket.rs
@@ -1,10 +1,10 @@
 use futures_util::{SinkExt, StreamExt};
 use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
-
 use tokio::net::TcpListener;
 use tokio::sync::broadcast;
 use tokio::sync::{mpsc, Mutex};
+use tokio::time::{interval, Duration};
 use tokio_tungstenite::{accept_async, tungstenite::protocol::Message, WebSocketStream};
 
 use crate::app::{App, MessageType};
@@ -52,14 +52,9 @@ async fn handle_connection(
 ) {
     let ws_stream: WebSocketStream<_> = accept_async(stream).await.expect("Error during handshake");
 
-    // Clone client_id and all Arc values to be used in async tasks
+    // Clone the necessary values
     let client_id = format!("{}", ws_stream.get_ref().peer_addr().unwrap());
-    let client_id_clone = client_id.clone(); // Clone for use inside the async block
-    let client_id_shutdown = client_id.clone(); // Separate clone for use in the shutdown message
-
-    let clients_clone = clients.clone(); // Clone Arc to share with async tasks
-    let history_clone = history.clone(); // Clone Arc for async tasks
-    let app_clone = app.clone(); // Clone Arc to use within the async block
+    let client_id_shutdown = client_id.clone(); // Clone for use in shutdown handling
 
     let (tx_original, mut rx) = mpsc::unbounded_channel();
 
@@ -73,78 +68,171 @@ async fn handle_connection(
         tx_original.send(message.clone()).unwrap();
     }
 
-    let (mut outgoing, mut incoming) = ws_stream.split();
+    let (outgoing, mut incoming) = ws_stream.split();
+    let outgoing = Arc::new(Mutex::new(outgoing));
 
-    // Task for sending messages to the client
-    let send_task = tokio::spawn(async move {
-        while let Some(message) = rx.recv().await {
-            let serialized_message = serde_json::to_string(&message).unwrap();
-            if outgoing
-                .send(Message::Text(serialized_message))
-                .await
-                .is_err()
-            {
-                break;
+    let disconnect_handled = Arc::new(Mutex::new(false));
+
+    // Task for sending periodic ping messages to the client
+    let ping_task = {
+        let outgoing_clone = Arc::clone(&outgoing);
+        let client_id_clone = client_id.clone();
+        let clients_clone = Arc::clone(&clients);
+        let app_clone = Arc::clone(&app);
+        let disconnect_handled_clone = Arc::clone(&disconnect_handled);
+
+        tokio::spawn(async move {
+            let mut interval = interval(Duration::from_secs(30));
+            loop {
+                interval.tick().await;
+                let mut outgoing_lock = outgoing_clone.lock().await;
+                if outgoing_lock.send(Message::Ping(vec![])).await.is_err() {
+                    break;
+                }
             }
-        }
-    });
+            println!("Ping task ended for client: {}", client_id_clone);
+            handle_disconnection(
+                disconnect_handled_clone,
+                &client_id_clone,
+                &clients_clone,
+                Arc::clone(&app_clone),
+            )
+            .await;
+        })
+    };
+
+    // Task for sending messages from the server to the client
+    let send_task = {
+        let outgoing_clone = Arc::clone(&outgoing);
+        let client_id_clone = client_id.clone();
+        let clients_clone = Arc::clone(&clients);
+        let app_clone = Arc::clone(&app);
+        let disconnect_handled_clone = Arc::clone(&disconnect_handled);
+
+        tokio::spawn(async move {
+            while let Some(message) = rx.recv().await {
+                let serialized_message = serde_json::to_string(&message).unwrap();
+                let mut outgoing_lock = outgoing_clone.lock().await;
+                if outgoing_lock
+                    .send(Message::Text(serialized_message))
+                    .await
+                    .is_err()
+                {
+                    break;
+                }
+            }
+            println!("Send task ended for client: {}", client_id_clone);
+            handle_disconnection(
+                disconnect_handled_clone,
+                &client_id_clone,
+                &clients_clone,
+                Arc::clone(&app_clone),
+            )
+            .await;
+        })
+    };
 
     // Task for receiving messages from the client
-    let recv_task = tokio::spawn(async move {
-        while let Some(Ok(Message::Text(text))) = incoming.next().await {
-            // Deserialize incoming message into MessageType
-            match serde_json::from_str::<MessageType>(&text) {
-                Ok(message) => {
-                    handle_incoming_message(
-                        message,
-                        &client_id_clone,
-                        &clients_clone,
-                        &app_clone,
-                        &history_clone,
-                    )
-                    .await
+    let recv_task = {
+        let client_id_clone = client_id.clone();
+        let clients_clone = Arc::clone(&clients);
+        let app_clone = Arc::clone(&app);
+        let history_clone = Arc::clone(&history);
+        let disconnect_handled_clone = Arc::clone(&disconnect_handled);
+
+        tokio::spawn(async move {
+            while let Some(result) = incoming.next().await {
+                match result {
+                    Ok(Message::Text(text)) => {
+                        // Log the received message
+                        println!("Received message from client {}: {}", client_id_clone, text);
+
+                        match serde_json::from_str::<MessageType>(&text) {
+                            Ok(message) => {
+                                handle_incoming_message(
+                                    message,
+                                    &client_id_clone,
+                                    &clients_clone,
+                                    &app_clone,
+                                    &history_clone,
+                                )
+                                .await
+                            }
+                            Err(_) => {
+                                println!("Invalid message format from client: {}", client_id_clone);
+                            }
+                        }
+                    }
+                    Ok(Message::Ping(_)) => {
+                        println!("Received Ping from client {}", client_id_clone);
+                    }
+                    Ok(Message::Pong(_)) => {
+                        println!("Received Pong from client {}", client_id_clone);
+                    }
+                    Ok(_) => {
+                        println!(
+                            "Received other type of message from client {}",
+                            client_id_clone
+                        );
+                    }
+                    Err(e) => {
+                        // Log any errors that occur
+                        println!(
+                            "Error receiving message from client {}: {}",
+                            client_id_clone, e
+                        );
+                        break; // Break the loop on error
+                    }
                 }
-                Err(_) => println!("Invalid message format from client {}", client_id_clone),
             }
-        }
+            println!("Recv task ended for client: {}", client_id_clone);
+            handle_disconnection(
+                disconnect_handled_clone,
+                &client_id_clone,
+                &clients_clone,
+                Arc::clone(&app_clone),
+            )
+            .await;
+        })
+    };
 
-        // Handle disconnection
-        handle_disconnect(&client_id_clone, &clients_clone, app_clone).await;
-    });
-
-    // Wait for shutdown or any of the tasks to complete
+    // Wait for shutdown or any task to complete
     tokio::select! {
         _ = send_task => {},
         _ = recv_task => {},
+        _ = ping_task => {},
         _ = shutdown.recv() => {
-            println!("Shutdown received for client: {}", client_id_shutdown); // Using separate clone here
+            println!("Shutdown received for client: {}", client_id_shutdown);
         }
     }
 
     // Handle disconnect after tasks are done
-    handle_disconnect(&client_id, &clients, app).await;
+    handle_disconnection(disconnect_handled, &client_id, &clients, app).await;
 }
 
-async fn handle_disconnect(
+// Handle disconnection logic (only once)
+async fn handle_disconnection(
+    disconnect_handled: Arc<Mutex<bool>>,
     client_id: &str,
     clients: &Arc<Mutex<HashMap<String, (Option<String>, mpsc::UnboundedSender<MessageType>)>>>,
     app: Arc<Mutex<App>>,
 ) {
-    // Remove the client from the active list
+    let mut handled = disconnect_handled.lock().await;
+    if *handled {
+        return; // Disconnection already handled
+    }
+    *handled = true;
+
     let client_name = {
         let mut clients_guard = clients.lock().await;
         let client_name = clients_guard.remove(client_id).and_then(|(name, _)| name);
-        client_name.unwrap_or_else(|| client_id.to_string()) // Use client_id if no name was set
+        client_name.unwrap_or_else(|| client_id.to_string())
     };
 
-    // Update App state to remove the disconnected user
-    app.lock().await.remove_connected_user(&client_id).await;
+    app.lock().await.remove_connected_user(client_id).await;
 
-    // Send a system message about the disconnection
     let disconnect_message =
         MessageType::SystemMessage(format!("{} has disconnected.", client_name));
-
-    // Broadcast the disconnection message to all remaining clients
     for (_, (_, tx)) in clients.lock().await.iter() {
         tx.send(disconnect_message.clone()).unwrap();
     }
